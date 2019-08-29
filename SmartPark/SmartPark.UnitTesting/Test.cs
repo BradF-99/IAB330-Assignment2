b9a43bd54e23e536d81d9e74f7849012803c5ce3
Brad Fuller <bfuller@everware.io>
2019-08-29 11:42:56 +1000

    Added unit testing projects

---
+++ SmartPark/SmartPark.UnitTesting/Test.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+namespace SmartPark.UnitTests
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+        }
+    }
+}
