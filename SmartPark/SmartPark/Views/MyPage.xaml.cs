f296b9f4880494cd73552472d7edb76045500f69
Brad Fuller <bfuller@everware.io>
2019-09-05 10:55:25 +1000

    Remade Xcode project
    It got corrupted for some reason

---
+++ SmartPark/SmartPark/Views/MyPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SmartPark.Views
+{
+    public partial class MyPage : ContentPage
+    {
+        public MyPage()
+        {
+            InitializeComponent();
+        }
+        async void OnButtonClicked(object sender, EventArgs args)
+        {
+            await label.RelRotateTo(360, 1000);
+        }
+    }
+}
