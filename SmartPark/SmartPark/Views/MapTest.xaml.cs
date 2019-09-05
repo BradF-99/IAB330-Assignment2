7bf9daf614e31d707e74e5faf3d7cf0d09129b97
Brad Fuller <bfuller@everware.io>
2019-09-05 12:51:41 +1000

    Added Location permission for iOS

---
+++ SmartPark/SmartPark/Views/MapTest.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace SmartPark.Views
+{
+    public partial class MapTest : ContentPage
+    {
+        public MapTest()
+        {
+            InitializeComponent();
+        }
+    }
+}
