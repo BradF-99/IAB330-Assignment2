e3be7cba97823f175aa563badc1a4f926b4d0457
Brad Fuller <bfuller@everware.io>
2019-09-05 13:18:58 +1000

    Added MapTest page

---
+++ SmartPark/SmartPark/Views/MapsTest.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace SmartPark.Views
+{
+    public partial class MapsTest : ContentPage
+    {
+        public MapsTest()
+        {
+            var map = new Map(
+            MapSpan.FromCenterAndRadius(
+                    new Position(-27.476944,153.028056), Distance.FromKilometers(1.0)))
+            {
+                IsShowingUser = true,
+                HeightRequest = 100,
+                WidthRequest = 960,
+                VerticalOptions = LayoutOptions.FillAndExpand
+            };
+            var stack = new StackLayout { Spacing = 0 };
+            stack.Children.Add(map);
+            Content = stack;
+
+        }
+    }
+}
