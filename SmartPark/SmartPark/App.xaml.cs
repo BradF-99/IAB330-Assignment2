206884521308f24f28b40013a8300a38f4248c48
Brad Fuller <bfuller@everware.io>
2019-09-19 12:46:57 +1000

    Changed initial view to MainPage

--- SmartPark/SmartPark/App.xaml.cs
+++ SmartPark/SmartPark/App.xaml.cs
@@ -24,7 +24,7 @@ namespace SmartPark
                 DependencyService.Register<MockDataStore>();
             else
                 DependencyService.Register<AzureDataStore>();
-            MainPage = new MapsTest();
+            MainPage = new MainPage();
         }
 
         protected override void OnStart()
