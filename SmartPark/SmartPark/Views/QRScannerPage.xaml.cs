c6a697b697337cd6ab13537844fe694cb866ad14
Brad Fuller <37586752+BradF-99@users.noreply.github.com>
2019-09-24 20:00:10 +1000

    Added QR Scanner Page

---
+++ SmartPark/SmartPark/Views/QRScannerPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using ZXing;
+
+namespace SmartPark.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class QRScannerPage : ContentPage
+    {
+        public QRScannerPage()
+        {
+            InitializeComponent();
+        }
+
+        public void Handle_OnScanResult(Result result)
+        {
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                await DisplayAlert("Scanned result", result.Text, "OK");
+            });
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _scanView.IsScanning = true;
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            _scanView.IsScanning = false;
+        }
+    }
+}
