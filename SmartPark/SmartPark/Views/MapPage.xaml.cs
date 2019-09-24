04082d4b75083e03aa79b3d753aa430a0f5fd2f3
Brad Fuller <37586752+BradF-99@users.noreply.github.com>
2019-09-24 19:59:46 +1000

    Renamed MapTest to MapPage

--- SmartPark/SmartPark/Views/MapPage.xaml.cs
+++ SmartPark/SmartPark/Views/MapPage.xaml.cs
@@ -5,23 +5,27 @@ using Xamarin.Forms.Maps;
 
 namespace SmartPark.Views
 {
-    public partial class MapsTest : ContentPage
+    public partial class MapPage : ContentPage
     {
-        public MapsTest()
+        public MapPage()
         {
             var map = new Map(
             MapSpan.FromCenterAndRadius(
                     new Position(-27.476944,153.028056), Distance.FromKilometers(1.0)))
             {
-                IsShowingUser = true,
                 HeightRequest = 100,
                 WidthRequest = 960,
                 VerticalOptions = LayoutOptions.FillAndExpand
             };
+
+            if(Device.RuntimePlatform == Device.Android) // we have to handle location perm ourselves
+            {
+                
+            }
+
             var stack = new StackLayout { Spacing = 0 };
             stack.Children.Add(map);
             Content = stack;
-
-        }
+         }
     }
 }
