3c27386fd7fb4b8a263593008551a9e87e4e1cbd
Brad Fuller <bfuller@everware.io>
2019-09-26 12:39:11 +1000

    Added code for redis handling

--- SmartPark-Backend/app.js
+++ SmartPark-Backend/app.js
@@ -1,20 +1,33 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var dataRouter = require('./routes/dataRouter');
+const indexRouter = require('./routes/index');
+const dataRouter = require('./routes/dataRouter');
 
-var app = express();
+const redisClient = require("./modules/redisHandler");
+
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/data', dataRouter);
+
+redisClient.on('connect', () => {
+    console.log("Successfully connected to Redis Server, we're ready!");
+});
+redisClient.on('error', err => {
+    console.log("Unable to connect to Redis, terminating.")
+    console.log(`Error: ${err}`);
+    process.exit(1);
+});
 
 module.exports = app;
