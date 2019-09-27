badc0ddd6559d0c4c23a31934b6e97542b4e5380
Brad Fuller <bfuller@everware.io>
2019-09-27 12:52:34 +1000

    Created new bindable map type

--- SmartPark/SmartPark/Views/MapPage.xaml.cs
+++ SmartPark/SmartPark/Views/MapPage.xaml.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using SmartPark.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Maps;
 
@@ -9,23 +13,69 @@ namespace SmartPark.Views
     {
         public MapPage()
         {
-            var map = new Map(
-            MapSpan.FromCenterAndRadius(
-                    new Position(-27.476944,153.028056), Distance.FromKilometers(1.0)))
-            {
-                HeightRequest = 100,
-                WidthRequest = 960,
-                VerticalOptions = LayoutOptions.FillAndExpand
-            };
+            BindingContext = new LocationViewModel();
+            InitializeComponent();
+        }
+    }
 
-            if(Device.RuntimePlatform == Device.Android) // we have to handle location perm ourselves
-            {
-                
-            }
+    public class BindableMap : Map
+    {
 
-            var stack = new StackLayout { Spacing = 0 };
-            stack.Children.Add(map);
-            Content = stack;
-         }
+        public static readonly BindableProperty MapPinsProperty = BindableProperty.Create(
+            nameof(Pins),
+            typeof(ObservableCollection<Pin>),
+            typeof(BindableMap),
+            new ObservableCollection<Pin>(),
+            propertyChanged: (b, o, n) =>
+            {
+                var bindable = (BindableMap)b;
+                bindable.Pins.Clear();
+
+                var collection = (ObservableCollection<Pin>)n;
+                foreach (var item in collection)
+                    bindable.Pins.Add(item);
+
+                collection.CollectionChanged += (sender, e) =>
+                {
+                    Device.BeginInvokeOnMainThread(() =>
+                    {
+                        switch (e.Action)
+                        {
+                            case NotifyCollectionChangedAction.Add:
+                            case NotifyCollectionChangedAction.Replace:
+                            case NotifyCollectionChangedAction.Remove:
+                                if (e.OldItems != null)
+                                    foreach (var item in e.OldItems)
+                                        bindable.Pins.Remove((Pin)item);
+
+                                if (e.NewItems != null)
+                                    foreach (var item in e.NewItems)
+                                        bindable.Pins.Add((Pin)item);
+                                break;
+                            case NotifyCollectionChangedAction.Reset:
+                                bindable.Pins.Clear();
+                                break;
+                        }
+                    });
+                };
+
+
+
+            });
+
+        public IList<Pin> MapPins { get; set; }
+
+        public static readonly BindableProperty MapPositionProperty = BindableProperty.Create(
+                nameof(MapPosition),
+                typeof(Position),
+                typeof(BindableMap),
+                new Position(0, 0),
+                propertyChanged: (b, o, n) =>
+                {
+                    ((BindableMap)b).MoveToRegion(MapSpan.FromCenterAndRadius(
+                                (Position)n, Distance.FromKilometers(1)));
+
+                });
+        public Position MapPosition { get; set; }
     }
 }
