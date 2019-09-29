6618c0418c4fc5d7f9d19d97e36bff2a99e21cca
Brad Fuller <37586752+BradF-99@users.noreply.github.com>
2019-09-30 02:11:43 +1000

    Completed API Data Store

---
+++ SmartPark/SmartPark/Services/APIDataStore.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using SmartPark.Models;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace SmartPark.Services
+{
+    public class APIDataStore
+    {
+        public List<ParkingMeter> meters;
+        public APIDataStore()
+        {
+            meters = new List<ParkingMeter>();
+            string url = "http://149.28.165.104:3000/data/";
+            var json = new WebClient().DownloadString(url);
+
+            meters = JsonConvert.DeserializeObject<List<ParkingMeter>>(json);
+        }
+    }
+}
