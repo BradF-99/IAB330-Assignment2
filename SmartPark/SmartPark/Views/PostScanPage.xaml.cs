61704f8e2fe34ac93de1ec001cdd80665ef2fb94
Brad Fuller <37586752+BradF-99@users.noreply.github.com>
2019-09-30 02:52:14 +1000

    Created new page for successful QR scan

---
+++ SmartPark/SmartPark/Views/PostScanPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using SmartPark.Models;
+using SmartPark.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using ZXing;
+
+namespace SmartPark.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PostScanPage : ContentPage
+    {
+        public PostScanPage(Result result)
+        {
+            ParkingMeter meter = APIDataStore.getData(result.Text);
+            string labelTest = "Meter Details: " + meter.STREET + ", " + meter.SUBURB + "(" + meter.METER_NO + ")";
+            labelDetails.BindingContext = labelTest;
+            labelDetails.SetBinding(Label.TextProperty, labelTest);
+            InitializeComponent();
+        }
+    }
+}
