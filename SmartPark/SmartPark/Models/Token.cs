a9dd4cb6e00f972fa1fbb762ea35436c3928d4c5
Cody <codydarlington1@gmail.com>
2019-09-29 13:48:42 +1000

    Class for user login information

---
+++ SmartPark/SmartPark/Models/Token.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace SmartPark.Models
+{
+    public class Token
+    {
+        public int Id { get; set; }
+        public string Access_token { get; set; }
+        public string Error_description { get; set; }
+        public DateTime Expire_date { get; set; }
+    }
+}
--- SmartPark/SmartPark/Views/LoginPage.xaml.cs
+++ SmartPark/SmartPark/Views/LoginPage.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using SmartPark.Models;
 using Xamarin.Forms;
 
 namespace SmartPark.Views
@@ -11,10 +11,19 @@ namespace SmartPark.Views
         {
             InitializeComponent();
         }
-        private async void LoginAccepted(object sender, EventArgs e)
+        void LoginAccepted(object sender, EventArgs e)
         {
-            await Navigation.PopAsync();
-            await Navigation.PushAsync(new MainPage());
+            //await Navigation.PopAsync();
+            //await Navigation.PushAsync(new MainPage());
+            User user = new User(User_email.Text, User_password.Text);
+            if(user.CheckInformation())
+            {
+                DisplayAlert("Login", "Login Success", "Oke");
+            }
+            else
+            {
+                DisplayAlert("Login", "Login Not Correct, Empty Username or Password", "Oke");
+            }
         }
     }
 }
