b4578ec0763c6c979725ff7b2ad432b0c5ca6aeb
Charly <50072097+CharlyShin@users.noreply.github.com>
2019-09-30 03:01:48 +1000

    fixed AccountPage as login page

--- SmartPark/SmartPark/Views/AccountPage.xaml.cs
+++ SmartPark/SmartPark/Views/AccountPage.xaml.cs
@@ -1,6 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-
+using SmartPark.Models;
 using Xamarin.Forms;
 
 namespace SmartPark.Views
@@ -10,6 +10,19 @@ namespace SmartPark.Views
         public AccountPage()
         {
             InitializeComponent();
+        }
+        async void LoginAccepted(object sender, EventArgs e)
+        {
+            User user = new User(User_email.Text, User_password.Text);
+            if(user.CheckInformation())
+            {
+                Application.Current.MainPage = new MainPage();
+
+            }
+            else
+            {
+                await DisplayAlert("Login", "Login Not Correct, Empty Username or Password", "Ok");
+            }
         }
     }
 }
