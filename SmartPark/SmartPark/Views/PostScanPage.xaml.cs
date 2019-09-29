4cbe6dbdc6c1e4aa65372e9058c75442dbdd20a3
Brad Fuller <37586752+BradF-99@users.noreply.github.com>
2019-09-30 02:54:56 +1000

    Temporarily removed binding contexts

--- SmartPark/SmartPark/Views/PostScanPage.xaml.cs
+++ SmartPark/SmartPark/Views/PostScanPage.xaml.cs
@@ -19,8 +19,8 @@ namespace SmartPark.Views
         {
             ParkingMeter meter = APIDataStore.getData(result.Text);
             string labelTest = "Meter Details: " + meter.STREET + ", " + meter.SUBURB + "(" + meter.METER_NO + ")";
-            labelDetails.BindingContext = labelTest;
-            labelDetails.SetBinding(Label.TextProperty, labelTest);
+            //labelDetails.BindingContext = labelTest;
+            //labelDetails.SetBinding(Label.TextProperty, labelTest);
             InitializeComponent();
         }
     }
