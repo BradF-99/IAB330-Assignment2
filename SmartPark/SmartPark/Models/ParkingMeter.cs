04d96b06ce0b106f5b2a584bc24912a7e659da67
Brad Fuller <37586752+BradF-99@users.noreply.github.com>
2019-09-29 23:50:52 +1000

    Updated meter class to reflect json output

--- SmartPark/SmartPark/Models/ParkingMeter.cs
+++ SmartPark/SmartPark/Models/ParkingMeter.cs
@@ -4,21 +4,26 @@ using System.Text;
 
 namespace SmartPark.Models
 {
-    class ParkingMeter
+    public class ParkingMeter
     {
-        public int ID { get; set; }
-        public string description { get; set; }
-        public double Latitude { get; set; }
-        public double Longitude { get; set; }
-        public string Category { get; set; }
-        public string Street { get; set; }
-        public string Suburb { get; set; }
-        public int HoursMax { get; set; }
-        public double RateWkday { get; set; }
-        public double RateWkend { get; set; }
-        public int baysCar { get; set; }
-        public int baysMotorcycle { get; set; }
-        public string operationalDays { get; set; }
-        public string operationalTimes { get; set; }
+        public string METER_NO { get; set; }
+        public string CATEGORY { get; set; }
+        public string STREET { get; set; }
+        public string SUBURB { get; set; }
+        public string MAX_STAY_HRS { get; set; }
+        public string RESTRICTIONS { get; set; }
+        public string OPERATIONAL_DAY { get; set; }
+        public string OPERATIONAL_TIME { get; set; }
+        public string TAR_ZONE { get; set; }
+        public string TAR_RATE_WEEKDAY { get; set; }
+        public string TAR_RATE_AH_WE { get; set; }
+        public string LOC_DESC { get; set; }
+        public string VEH_BAYS { get; set; }
+        public string MC_BAYS { get; set; }
+        public string MC_RATE { get; set; }
+        public string LONGITUDE { get; set; }
+        public string LATITUDE { get; set; }
+        public string MOBILE_ZONE { get; set; }
+        public string MAX_CAP_CHG { get; set; }
     }
 }
