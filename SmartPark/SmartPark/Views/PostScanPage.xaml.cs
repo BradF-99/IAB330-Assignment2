b1f5a31337a77cff81a1f3a3f117d79d3f0bb73f
Brad Fuller <37586752+BradF-99@users.noreply.github.com>
2019-09-30 12:22:01 +1000

    Replaced test label with formatted string

--- SmartPark/SmartPark/Views/PostScanPage.xaml.cs
+++ SmartPark/SmartPark/Views/PostScanPage.xaml.cs
@@ -15,17 +15,32 @@ namespace SmartPark.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class PostScanPage : ContentPage
     {
-        public string labelResult { get; set; }
-
         public PostScanPage(Result result)
         {
             ParkingMeter meter = APIDataStore.getData(result.Text);
+            
+            var fs = new FormattedString();
 
-            string resultText = "Meter Details: " + meter.STREET + ", " + meter.SUBURB + "(" + meter.METER_NO + ")";
+            fs.Spans.Add(new Span { Text = "Meter ID: ", FontAttributes = FontAttributes.Bold, FontSize = 20 });
+            fs.Spans.Add(new Span { Text = meter.METER_NO + "\n", FontSize = 20 });
 
-            //labelDetails.SetBinding(Label.TextProperty, labelTest);
+            fs.Spans.Add(new Span { Text = "Location: ", FontAttributes = FontAttributes.Bold, FontSize = 20 });
+            fs.Spans.Add(new Span { Text = meter.STREET + " " + meter.SUBURB + "\n", FontSize = 20 });
+
+            fs.Spans.Add(new Span { Text = "Bays available: ", FontAttributes = FontAttributes.Bold, FontSize = 20 });
+            fs.Spans.Add(new Span { Text = meter.VEH_BAYS, FontAttributes = FontAttributes.Italic, FontSize = 20 });
+            fs.Spans.Add(new Span { Text = " car bays & ", FontSize = 20 });
+            fs.Spans.Add(new Span { Text = meter.MC_BAYS, FontAttributes = FontAttributes.Italic, FontSize = 20 });
+            fs.Spans.Add(new Span { Text = " motorcycle bays. \n", FontSize = 20 });
+
+            fs.Spans.Add(new Span { Text = "Maximum Stay: ", FontAttributes = FontAttributes.Bold, FontSize = 20 });
+            fs.Spans.Add(new Span { Text = meter.MAX_STAY_HRS, FontAttributes = FontAttributes.Italic, FontSize = 20 });
+            fs.Spans.Add(new Span { Text = " hours, and it costs $", FontSize = 20 });
+            fs.Spans.Add(new Span { Text = meter.TAR_RATE_WEEKDAY, FontAttributes = FontAttributes.Italic, FontSize = 20 });
+            fs.Spans.Add(new Span { Text = " per hour to park here.", FontSize = 20 });
+            
             InitializeComponent();
-            labelDetails.Text = resultText;
+            labelDetails.FormattedText = fs;
         }
     }
 }
