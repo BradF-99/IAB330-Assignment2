cda48fc90372ba514a5ad8e9d4b8c6c085ac95ec
Brad Fuller <37586752+BradF-99@users.noreply.github.com>
2019-09-30 19:14:07 +1000

    Removed unused imports

--- SmartPark/SmartPark/Views/QRScannerPage.xaml.cs
+++ SmartPark/SmartPark/Views/QRScannerPage.xaml.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using ZXing;
 using ZXing.Net.Mobile.Forms;
 
 namespace SmartPark.Views
