1a0145c6e7a4b90ea4da084d725c57e145b434c6
Brad Fuller <37586752+BradF-99@users.noreply.github.com>
2019-09-30 15:40:45 +1000

    Fixed Android QR scan freezing after scan

--- SmartPark/SmartPark/Views/QRScannerPage.xaml.cs
+++ SmartPark/SmartPark/Views/QRScannerPage.xaml.cs
@@ -7,37 +7,76 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using ZXing;
+using ZXing.Net.Mobile.Forms;
 
 namespace SmartPark.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class QRScannerPage : ContentPage
     {
+        ZXingScannerView scanner;
         public QRScannerPage()
         {
             InitializeComponent();
-            _scanView.IsScanning = false;
-        }
-
-        public void Handle_OnScanResult(Result result)
-        {
-            _scanView.IsScanning = false;
-            Device.BeginInvokeOnMainThread(async () =>
-            {
-                await Navigation.PushAsync(new PostScanPage(result));
-            });
+            ScannerGrid.VerticalOptions = LayoutOptions.FillAndExpand;
+            ScannerGrid.HorizontalOptions = LayoutOptions.FillAndExpand;
+            CreateScanner();
         }
 
         protected override void OnAppearing()
         {
             base.OnAppearing();
-            _scanView.IsScanning = true;
+            CreateScanner();
         }
 
         protected override void OnDisappearing()
         {
             base.OnDisappearing();
-            _scanView.IsScanning = false;
+            ReleaseScanner();
+        }
+
+        void CreateScanner()
+        {
+            ReleaseScanner();
+
+            Device.BeginInvokeOnMainThread(() => {
+                scanner = new ZXingScannerView
+                {
+                    HorizontalOptions = LayoutOptions.FillAndExpand,
+                    VerticalOptions = LayoutOptions.FillAndExpand,
+                    AutomationId = "zxingScannerView",
+                    IsScanning = true,
+                    IsAnalyzing = true
+                };
+                scanner.OnScanResult += Scanner_OnScanResult;
+                ScannerGrid.Children.Add(scanner);
+            });
+        }
+
+        void ReleaseScanner()
+        {
+            Device.BeginInvokeOnMainThread(() => {
+                if (scanner != null)
+                {
+                    scanner.IsTorchOn = false;
+                    scanner.IsAnalyzing = false;
+                    scanner.IsScanning = false;
+                    scanner.IsVisible = false;
+                    scanner.OnScanResult -= Scanner_OnScanResult;
+                    ScannerGrid.Children.Remove(scanner);
+                    scanner = null;
+                }
+            });
+        }
+
+        void Scanner_OnScanResult(ZXing.Result result)
+        {
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                ReleaseScanner();
+                scanner.IsAnalyzing = false;
+                await Navigation.PushAsync(new PostScanPage(result));
+            });
         }
     }
 }
