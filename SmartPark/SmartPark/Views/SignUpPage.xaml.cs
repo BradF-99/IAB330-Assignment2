8e77735788259018ab1bb6bb46c716f5551d1d7d
Dan-Jung01 <54402002+Dan-Jung01@users.noreply.github.com>
2019-09-30 22:04:14 +1000

    sign in page
    sign in page

--- SmartPark/SmartPark/Views/SignUpPage.xaml.cs
+++ SmartPark/SmartPark/Views/SignUpPage.xaml.cs
@@ -11,5 +11,14 @@ namespace SmartPark.Views
         {
             InitializeComponent();
         }
+        async void OnButtonClicked(object sender, EventArgs args)
+        {
+            await DisplayAlert("Notice", "You have been signed up successfully, please press go back button", "OK");
+        }
+
+        private Page AccountPage()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
