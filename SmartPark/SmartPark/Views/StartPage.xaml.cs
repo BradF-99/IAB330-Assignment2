45e68da26ecfd37ed8a70d0a003d8d6545748679
Charly <50072097+CharlyShin@users.noreply.github.com>
2019-09-30 21:54:31 +1000

    Set StartPage as showing first open

---
+++ SmartPark/SmartPark/Views/StartPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using SmartPark.Models;
+using Xamarin.Forms;
+
+namespace SmartPark.Views
+{
+    public partial class StartPage : ContentPage
+    {
+        public StartPage()
+        {
+            InitializeComponent();
+        }
+
+        void Start_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new LoginPage());
+        }
+    }
+}
