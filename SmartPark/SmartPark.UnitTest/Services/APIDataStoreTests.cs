f3fb2b37a8ca02eabe8ffcb4e0b64e564e4b5d3c
Brad Fuller <37586752+BradF-99@users.noreply.github.com>
2019-11-04 15:31:57 +1000

    Addeed unit tests for Parking Meter model and API service

---
+++ SmartPark/SmartPark.UnitTest/Services/APIDataStoreTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SmartPark.Models;
+using SmartPark.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartPark.Services.Tests
+{
+    [TestClass()]
+    public class APIDataStoreTests
+    {
+        [TestMethod()]
+        public void APIDataStoreTest()
+        {
+            APIDataStore apiDataStore = new APIDataStore();
+            Assert.IsNotNull(apiDataStore); // fail if nothing returned
+            Assert.AreEqual(984, apiDataStore.meters.Count); // make sure all parking meters have been parsed
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void GetDataInvalidQueryTest()
+        {
+            ParkingMeter meter = APIDataStore.GetData("4000");
+        }
+
+        [TestMethod()]
+        public void GetDataValidQueryTest()
+        {
+            try
+            {
+                ParkingMeter meter = APIDataStore.GetData("5601");
+                Assert.IsNotNull(meter);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+    }
+}
